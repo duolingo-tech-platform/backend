91ba7fcc95d59cfcaeb324e3aed2b2519e82bbef
Maria Eduarda Claro <eduardaclaro@MacBook-Air-de-Maria.local>
2026-05-08 21:30:41 -0300

    :sparkles: feat: Adding User model with properties

---
+++ src/Models/User.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace DuolingoTechPlatform.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public int XP { get; set; }
+        public int Level { get; set; }
+        public int Streak { get; set; }
+    }
+}
